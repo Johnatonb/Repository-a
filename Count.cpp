e80b7237c3c01c3588fe4b59d6e572e6e9be339e
Johnatonb <johnkbrubaker@gmail.com>
2016-10-13 23:19:45 -0500

    Update Count.cpp

--- Count.cpp
+++ Count.cpp
@@ -1,25 +1,13 @@
 # include <iostream>
 # include <chrono>
+# include <thread>
 using namespace std;
 
 int main(){
     int desiredTime = 0;
-    cin >> desiredTime;
-    chrono::high_resolution_clock     hi_res_clock;
-     chrono::steady_clock              monotonic_clock;
-      chrono::system_clock              wall_clock;
-    for(int desiredTime;desiredTime>0;desiredTime--)
-        chrono::time_point<chrono::steady_clock, chrono::duration<int,nano>> lastTime;
-        chrono::time_point<chrono::steady_clock, chrono::duration<int,nano>> currentTime;
-        currentTime = hi_res_clock.now();
-        lastTime = currentTime;
-        chrono::duration<int,nano> timeDelta = currentTime - lastTime;
-        while(timeDelta<1ms){
-            currentTime = hi_res_clock.now();
-            timeDelta =currentTime-lastTime;
-        }
-        cout<<"There are "<<desiredTime<<" seconds left."
-        
+    for(cin>>desiredTime;desiredTime>0;desiredTime--){
+        std::this_thread::sleep_for (std::chrono::seconds(1));
+        cout<<"There are "<<desiredTime<<" seconds left."<<endl;
     }
     return 0;
 }
